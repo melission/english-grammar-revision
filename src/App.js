8eddfbb06526df799732c5f5235eb40accc9f0ce
Polina <polina@blsv.space>
2022-06-18 13:27:19 +0300

    built the whole logic: a function, import, and calculation.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import book from './book-vector.png'
+import rules from './rules.js'
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+    let [rule, setRule] = useState(rules[Math.floor((Math.random() * rules.length))]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div  className="App">
+          <header className="App-header">
+              <img src={book} className="App-logo" alt="book image"/>
+          </header>
+
+          <div className="App-info_block">
+              <div className="info-block_grammar-rule">
+                  {rule}
+
+              </div>
+              <div className="info-block_button">
+                  <button className="button" onClick={() => setRule(
+                      rule = rules[Math.floor((Math.random() * rules.length))])}>Get a rule</button>
+              </div>
+          </div>
+          <footer className="App-footer">
+            <p className="footer-info">Polin Belousova. 2022</p>
+          </footer>
+      </div>
+      )
 }
 
 export default App;
