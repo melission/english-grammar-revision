6998cb489ce13c38a5e048754edc9a578a17e917
Polina <polina@blsv.space>
2022-06-19 13:57:41 +0300

    created, rules are [] with [], [] inside.

---
+++ src/double_rules.js
@@ -0,0 +1,48 @@
+const double_rules = [
+    [['Modals of speculations'], ['can - is not used to make speculation\n;' +
+        'could - you are not sure (50/50);\n' +
+        'could do , could be , could be doing - unsure about the present\n' +
+        'could have done, could have been doing - about the past']],
+    [['Modals of speculations when you are sure: must, can'], ['He must be home now. \n' +
+    'He must have been home yesterday. \n' +
+    'must + V1 (must be) - you`re sure about sth positive in the present\n' +
+    'must + have + V3 (must have been) you`re sure about sth positive in the past \n' +
+    'can`t + V1 you`re sure about sth negative in the  present\n' +
+    'He can`t be home. (I`m sure he is not) \n' +
+    'can`t + have + V3 you`re sure about sth negative in the past \n' +
+    'he can`t have been home yesterday because he`s in Moscow \n']],
+    [['Modals of speculations when you aren\'t sure: could'], ['could / may / might  - you are not sure \n' +
+    'could do / may do / might do - present \n' +
+    'could be doing - continuous \n' +
+    'could have done / may have done / might have done - past']],
+    [['Modals of speculations: criticise and advice'], ['should do / ought to do - advice\n' +
+    'should have done / ought to have done - criticism']],
+    [['Do or Make'], ['Do is used when speaking about processes without focusing on the result. Do harm and do job. '
+    + 'Make focuses on the result. For example, make a choice and make a care. Both actions are completed.']],
+    [['The pronunciation of C'], ['In words which start with \'ca\', \'co\', \'cu\' the letter \'c\' is being ' +
+    'pronounced as [k]. For example: cat, corn, and cucumber. In words which start with \'ci\', \'ce\', and ' +
+    '\'cy\' the letter \'c\' is being pronounced like [s]. For example: \'city\', \'century\', and \'cycle\'. ']],
+    [["The pronunciation of G"], ["In words which start with 'ga', 'go', and 'gu', it's [g] sound. For example, 'gauge'," +
+    "'gold' and 'guild'. Yet in words which start with 'ge', 'gy', and 'gi', it's [dʒ] sounds. For example, " +
+    "'gentle', 'gym', and 'gipsy'. "]],
+    [['My or Mine'], ["'My' is used to show the fact of the possession of a noun, while 'mine' shows the fact of " +
+    "possession itself and need no noun after it. For example: this is my husband but this husband is mine. "]],
+    [['Double consonants'], ["The last consonant is being doubled in two cases. If it's three letter word with a " +
+    "structure consonant-vowel-consonant. For example: red -> redder; wet -> wetter; stop -> stopped." +
+    "If it's a two syllable word with the stress on the second syllable and the second syllable has a C-V-C structure. " +
+    "For example: preFÉR -> preferred; énter -> entered; admit -> admitted. "]],
+    [["The usage of None"], ["As 'mine', none does not need a noun after and none works similar to no one. None wears masks is equal" +
+    " to no one wears masks and no people wear masks. "]],
+    [["Gradable and Non-gradable adjectives"], ["If an adjective has no meaning of 'very' in it, it's gradable. " +
+    "For example, tasty is gradable, but delicious [very tasty] is non-gradable. Non-gradable adjectives have grades " +
+    "already, so no gradable adverbs are allowed. For example, stunning -> very beautiful, freezing -> very cold. If you " +
+    "need to check whether an adjective is gradable check the meaning in Cambridge dictionary."]],
+    [["Gradable and Non-gradable adverbs"], ["If an adverb has the meaning of 'very' or other gradations, it's gradable " +
+    "and can be used with gradable adjectives. For example: very, extremely, a bit, unusually, slightly, fantastically," +
+    " immensely. If an adverb has a completed meaning, it's non-gradable. For example, completely, totally, absolutely. " +
+    "Really, quite, and pretty can be both: gradable and non-gradable adverbs. Really beautiful and really stunning. " +
+    "Almost, nearly, and virtually are non-gradable only. Rather is exclusively gradable. Utterly can be used with " +
+    "gradable and non-gradable adjectives. "]],
+]
+
+export default double_rules;
