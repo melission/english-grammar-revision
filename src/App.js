0088a87ea02eb292b3e0ecd3ef08c0e3fed4967c
Polina <polina@blsv.space>
2022-06-19 13:58:40 +0300

    added import of 'double_rules.js', replaced 'rules' with double_rules everywhere.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import book from './book-vector.png'
-import rules from './rules.js'
+import book from './book-vector.png';
+import double_rules from "./double_rules";
 import './App.css';
 import {useState} from "react";
 
 function App() {
 
-    let [rule, setRule] = useState(rules[Math.floor((Math.random() * rules.length))]);
+    // let [rule, setRule] = useState(rules[Math.floor((Math.random() * rules.length))]);
+    let [doubleRule, setDoubleRule] = useState(double_rules[Math.floor((Math.random() * double_rules.length))]);
 
   return (
       <div  className="App">
           <header className="App-header">
               <img src={book} className="App-logo" alt="book image"/>
           </header>
-
           <div className="App-info_block">
               <div className="info-block_grammar-rule">
-                  {rule}
-
+                  {/*{rule}*/}
+                  <h3>{doubleRule[0]}</h3>
+                  <p>{doubleRule[1]}</p>
               </div>
               <div className="info-block_button">
-                  <button className="button" onClick={() => setRule(
-                      rule = rules[Math.floor((Math.random() * rules.length))])}>Get a rule</button>
+                  {/*<button className="button" onClick={() => setRule(*/}
+                  {/*    rule = rules[Math.floor((Math.random() * rules.length))])}>Get a rule</button>*/}
+                  <button className="button" onClick={() => setDoubleRule(
+                      doubleRule = double_rules[Math.floor((Math.random() * doubleRule.length))])}>Get a rule</button>
               </div>
           </div>
           <footer className="App-footer">
